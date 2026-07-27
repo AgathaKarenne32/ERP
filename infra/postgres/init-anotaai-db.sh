cccb325ecf84974d02f4772490f39322081c657c
Agatha Karenne <142344001+AgathaKarenne32@users.noreply.github.com>
2026-07-27 14:43:25 -0300

    Revert "Feature/movimentacao estoque manual"

--- infra/postgres/init-anotaai-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Cria o segundo banco (anotaai) no mesmo container Postgres.
-# O banco 'ecletica' já é criado automaticamente pela imagem via POSTGRES_DB.
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE anotaai;
-EOSQL
